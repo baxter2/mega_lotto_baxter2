956604ca6e5a3d9579af99c0c0b0e50ba614ea55
mikaelblomkvist50 <mikaelblomkvist@gmail.com>
2018-10-09 20:07:39 +1000

    Remove redundant test

--- spec/mega_lotto_baxter2_spec.rb
+++ spec/mega_lotto_baxter2_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe MegaLottoBaxter2 do
   it "has a version number" do
     expect(MegaLottoBaxter2::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
