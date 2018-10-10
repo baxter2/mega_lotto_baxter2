3336fe378360d59b6528d988e5a1ad06ed9f673c
mikaelblomkvist50 <mikaelblomkvist@gmail.com>
2018-10-10 14:24:52 +1000

    The spec will assert after running the configuration block the #draw method returns an array like before but this time with 10 integers instead.

--- spec/mega_lotto_baxter2_spec.rb
+++ spec/mega_lotto_baxter2_spec.rb
@@ -3,3 +3,21 @@ RSpec.describe MegaLotto do
     expect(MegaLotto::VERSION).not_to be nil
   end
 end
+
+
+RSpec.describe MegaLotto do
+  describe "#configure" do
+    before do
+      MegaLotto.configure do |config|
+        config.drawing_count = 10
+      end
+    end
+ 
+    it "returns an array with 10 elements" do
+      draw = MegaLotto::Drawing.new.draw
+ 
+      expect(draw).to be_a(Array)
+      expect(draw.size).to eq(10)
+    end
+  end
+end
